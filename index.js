f99a2ae72d2fbdda6f0375df26c6bd5315d4f14a
malakanaresh <malakanaresh330@gmail.com>
2023-02-17 16:43:55 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="container">
+    <h1 className="greeting">Super Over League</h1>
+    <div className="bg-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="rcb"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="rcb"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
